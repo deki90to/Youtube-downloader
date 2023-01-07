c946721286dd6953de4abba3d4755d138f8e64bf
Dejan <deki90to@gmail.com>
2023-01-07 13:18:00 +0100

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,15 +1,13 @@
 from tkinter import *
 import tkinter.messagebox
 from pytube import YouTube
-from sys import argv
 import os
 
 root = Tk()
-root.geometry('400x150')
+root.geometry('400x140')
 root['bg']='#004038'
 root.title('Youtube Downloader')
-
-
+root.resizable(False, False)
 
 def clear_text():
    get_video_input.delete(0, END)
@@ -18,31 +16,28 @@ def clear_text():
 input_video_url = StringVar()
 input_audio_url = StringVar()
 
-
 def download(get_video_link=input_video_url.get() if input_video_url else None, get_audio_link=input_audio_url.get() if input_audio_url else None):
 
     video_link = input_video_url.get()
     audio_link = input_audio_url.get()
-
 
     if video_link:
         yt_video = YouTube(video_link)
         print('\nStart downloading... Please wait...\n')
 
         yvd = yt_video.streams.get_highest_resolution()
-        yvd.download() #example "C:\\Users\\Username\\Downloads\\Videos", by default downloaded-files are saved next to py file
+        yvd.download("C:\\Users\\Dejan\\Downloads\\Videos")
 
         print(f'\n{yt_video.title} Downloaded successfully')
         tkinter.messagebox.showinfo("Download finished", f"{yt_video.title} is saved on your pc")
         clear_text()
-
 
     if audio_link:
         yt_audio = YouTube(audio_link)
         print('\nStart downloading... Please wait...\n')
 
         yad = yt_audio.streams.filter(only_audio=True).first()
-        out_file = yad.download() #example "C:\\Users\\Username\\Downloads\\Audio", by default downloaded-files are saved next to py file
+        out_file = yad.download("C:\\Users\\Dejan\\Downloads\\Audios")
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
         os.rename(out_file, new_file)
@@ -51,11 +46,9 @@ def download(get_video_link=input_video_url.get() if input_video_url else None, 
         tkinter.messagebox.showinfo("Download finished", f"{yt_audio.title} is saved on your pc")
         clear_text()
 
-
     if 'https://www.youtube.com/' not in video_link and 'https://www.youtube.com/' not in audio_link:
         tkinter.messagebox.showinfo("File not found", "Choose video-url to download")
         clear_text()
-
 
 #VIDEO
 video_label = Label(root, text = 'Video Downloader', bg = '#004038', fg = 'white').pack()
