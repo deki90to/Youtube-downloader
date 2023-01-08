af1234bef4d6f3418eb6babec1b7389febc66f50
Dejan <deki90to@gmail.com>
2023-01-08 03:28:39 +0100

    try, except, result_label.config

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -4,7 +4,7 @@ from pytube import YouTube
 import os
 
 root = Tk()
-root.geometry('400x140')
+root.geometry('400x160')
 root['bg']='#004038'
 root.title('Youtube Downloader')
 root.resizable(False, False)
@@ -21,33 +21,42 @@ def download(get_video_link=input_video_url.get() if input_video_url else None, 
     video_link = input_video_url.get()
     audio_link = input_audio_url.get()
 
-    if video_link:
-        yt_video = YouTube(video_link)
-        print('\nStart downloading... Please wait...\n')
+    try:
+        if video_link:
+            yt_video = YouTube(video_link)
+            print('\nStart downloading... Please wait...\n')
+            # yvd = yt_video.streams.first()
+            yvd = yt_video.streams.get_highest_resolution()
+            yvd.download("C:\\Users\\Dejan\\Downloads\\Videos")
 
-        yvd = yt_video.streams.get_highest_resolution()
-        yvd.download("C:\\Users\\Dejan\\Downloads\\Videos")
+            print(f'\n{yt_video.title} Downloaded successfully')
+            result_label.config(text=f"{yt_video.title} video downloaded")
+            # tkinter.messagebox.showinfo("Download finished", f"{yt_video.title} is saved on your pc")
+            clear_text()
+    except:
+        result_label.config(text="An error occurred. Please check the file and try again.")
 
-        print(f'\n{yt_video.title} Downloaded successfully')
-        tkinter.messagebox.showinfo("Download finished", f"{yt_video.title} is saved on your pc")
-        clear_text()
+    try:
+        if audio_link:
+            yt_audio = YouTube(audio_link)
+            print('\nStart downloading... Please wait...\n')
 
-    if audio_link:
-        yt_audio = YouTube(audio_link)
-        print('\nStart downloading... Please wait...\n')
+            yad = yt_audio.streams.filter(only_audio=True).first()
+            out_file = yad.download("C:\\Users\\Dejan\\Downloads\\Audios")
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
 
-        yad = yt_audio.streams.filter(only_audio=True).first()
-        out_file = yad.download("C:\\Users\\Dejan\\Downloads\\Audios")
-        base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
-        os.rename(out_file, new_file)
-
-        print(f'\n{yt_audio.title} Downloaded successfully')
-        tkinter.messagebox.showinfo("Download finished", f"{yt_audio.title} is saved on your pc")
-        clear_text()
+            print(f'\n{yt_audio.title} Downloaded successfully')
+            result_label.config(text=f"{yt_audio.title} audio downloaded")
+            # tkinter.messagebox.showinfo("Download finished", f"{yt_audio.title} is saved on your pc")
+            clear_text()
+    except:
+        result_label.config(text="An error occurred. Please check the file and try again.")
 
     if 'https://www.youtube.com/' not in video_link and 'https://www.youtube.com/' not in audio_link:
-        tkinter.messagebox.showinfo("File not found", "Choose video-url to download")
+        result_label.config(text="You didn't choose video-url")
+        # tkinter.messagebox.showinfo("File not found", "Choose video-url to download")
         clear_text()
 
 #VIDEO
@@ -61,11 +70,12 @@ get_audio_input = Entry(root, width = 100, textvar = input_audio_url)
 get_audio_input.pack()
 
 #CONFIRM
-download_video_button = Button(root, width = 80, bg = 'green', fg = 'white', relief = 'sunken', text = 'Download',  command = download).pack()
+Button(root, width = 80, bg = 'green', fg = 'white', relief = 'sunken', text = 'Download', command = download).pack()
 
 #CLEAR FIELDS
 Button(root, text="Clear", command=clear_text, font=('Helvetica bold',10)).pack(pady=5)
 
-
+result_label = Label(root, text="", bg='#004038', fg='white')
+result_label.pack()
 
 root.mainloop()
